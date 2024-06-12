48ff41b62cf22856f455336e595893375b472d8d
Hiep Nguyen <nxhiep.1996@gmail.com>
2024-06-12 17:55:36 +0700

    minor change

--- example/lib/screens/new_components/new_study_tab.dart
+++ example/lib/screens/new_components/new_study_tab.dart
@@ -8,31 +8,37 @@ class TestNewStudyTabScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     final subjectDataList = <SubjectData>[
       SubjectData(
+        '1',
         'assets/images/subject_icon.svg',
         'Arithmetic Reasoning',
         22,
       ),
       SubjectData(
+        '2',
         'assets/images/subject_icon.svg',
         'Assembling Objects',
         50,
       ),
       SubjectData(
+        '3',
         'assets/images/subject_icon.svg',
         'Tư Tưởng Hồ Chí Minh',
         100,
       ),
       SubjectData(
+        '4',
         'assets/images/subject_icon.svg',
         'Gangnam Style',
         80,
       ),
       SubjectData(
+        '5',
         'assets/images/subject_icon.svg',
         'Mathematics Knowledge',
         30,
       ),
       SubjectData(
+        '6',
         'assets/images/subject_icon.svg',
         'Haz Mat',
         70,
@@ -42,7 +48,10 @@ class TestNewStudyTabScreen extends StatelessWidget {
     return Scaffold(
         backgroundColor: const Color(0xFFF5F4EE),
         body: SafeArea(
-            child: NewStudyTab(
+          child: NewStudyTab(
+          onSelected: (item) {
+            
+          },
           buttonText: '',
           mainColor: const Color(0xFFE3A651),
           subjectDataList: subjectDataList,
