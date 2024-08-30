33834b6a36e03a1d4dcf096491842c2d9c1a760b
Nguyen Nhu Dai <dainn@Nguyens-Mac-mini.local>
2024-08-31 01:18:55 +0700

    test_navbar

--- lib/src/widgets/animations/experiment_anims.dart
+++ lib/src/widgets/animations/experiment_anims.dart
@@ -1,5 +1,6 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_abc_jsc_components/flutter_abc_jsc_components.dart';
+import 'package:flutter/widgets.dart';
 
 class ExperimentAnims extends StatefulWidget {
   const ExperimentAnims({super.key});
@@ -8,270 +9,131 @@ class ExperimentAnims extends StatefulWidget {
   State<ExperimentAnims> createState() => _ExperimentAnimsState();
 }
 
-class _ExperimentAnimsState extends State<ExperimentAnims> with TickerProviderStateMixin {
-  late AnimationController prevController;
-  late AnimationController nextController;
-
-  late Animation<double> prevScaleAnimation;
-  late Animation<double> prevFadeAnimation;
-
-  late Animation<double> nextTranslateAnimation;
-  late Animation<double> nextFadeAnimation;
-
-  late AnimationController oldQuestion;
-  late AnimationController newQuestion;
-
-  List<AnimationController> oldAnswers = [];
-  List<AnimationController> newAnswers = [];
-
-  late Animation<double> oldQuestionAnim;
-  late Animation<double> newQuestionAnim;
-
-  List<Animation<double>> oldAnswersAnim = [];
-  List<Animation<double>> newAnswersAnim = [];
-
-  @override
-  void initState() {
-    prevController = AnimationController(vsync: this, duration: const Duration(milliseconds: 200));
-    nextController = AnimationController(vsync: this, duration: const Duration(milliseconds: 200));
-
-    prevFadeAnimation = Tween<double>(begin: 1, end: 0).animate(prevController);
-    prevScaleAnimation = Tween<double>(begin: 1, end: 0.95).animate(prevController);
-
-    nextTranslateAnimation = Tween<double>(begin: 100, end: 0).animate(nextController);
-    nextFadeAnimation = Tween<double>(begin: 0, end: 1).animate(nextController);
-
-    oldQuestion = AnimationController(vsync: this, duration: const Duration(milliseconds: 200));
-    oldQuestionAnim = Tween<double>(begin: 0, end: -500).animate(oldQuestion);
-    newQuestion = AnimationController(vsync: this, duration: const Duration(milliseconds: 200));
-    newQuestionAnim = Tween<double>(begin: 500, end: 0).animate(newQuestion);
-
-    oldAnswers = List.generate(4, (_) => AnimationController(vsync: this, duration: const Duration(milliseconds: 200)));
-    oldAnswersAnim = List.generate(4, (index) => Tween<double>(begin: 0, end: -500).animate(oldAnswers[index]));
-    newAnswers = List.generate(4, (_) => AnimationController(vsync: this, duration: const Duration(milliseconds: 200)));
-    newAnswersAnim = List.generate(4, (index) => Tween<double>(begin: 500, end: 0).animate(newAnswers[index]));
-
-    super.initState();
-  }
-
-  @override
-  void dispose() {
-    prevController.dispose();
-    nextController.dispose();
-
-    oldQuestion.dispose();
-    newQuestion.dispose();
-
-    for (var controller in oldAnswers) {
-      controller.dispose();
-    }
-    for (var controller in newAnswers) {
-      controller.dispose();
-    }
-
-    super.dispose();
-  }
+class _ExperimentAnimsState extends State<ExperimentAnims> {
+  double offset = 0;
+  int itemCount = 4;
+  int currentIndex = 0;
 
   @override
   Widget build(BuildContext context) {
+    final sideBoxWidth = (MediaQuery.of(context).size.width - 218) / 2 + 10;
+    final initialDistance = MediaQuery.of(context).size.width / (itemCount * 2);
+    offset = initialDistance * (currentIndex * 2 + 1) - (MediaQuery.of(context).size.width / 2);
     return Scaffold(
-      backgroundColor: Colors.white,
-      appBar: AppBar(
-        title: const Text('ANIMATION LAB'),
-      ),
-      body: SafeArea(
-        child: Column(
-          children: [
-            Expanded(
-                child: SingleChildScrollView(
-              child: Stack(
+        body: Column(
+      children: [
+        Expanded(
+            child: Container(
+          color: Colors.blue,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
                 children: [
-                  // AnimatedBuilder(
-                  //   animation: prevController,
-                  //   builder: (_, __) => ScaleTransition(
-                  //     scale: prevScaleAnimation,
-                  //     child: Opacity(opacity: prevFadeAnimation.value, child: _buildDummyQuestion(Colors.red)),
-                  //   ),
-                  // ),
-                  // AnimatedBuilder(
-                  //   animation: nextController,
-                  //   builder: (_, __) => Transform.translate(
-                  //     offset: Offset(nextTranslateAnimation.value, 0),
-                  //     child: Opacity(
-                  //       opacity: nextFadeAnimation.value,
-                  //       child: _buildDummyQuestion(Colors.blue),
-                  //     ),
-                  //   ),
-                  // )
-
-                  Column(
-                    children: [
-                      AnimatedBuilder(
-                        animation: oldQuestion,
-                        builder: (_, __) => Transform.translate(
-                          offset: Offset(oldQuestionAnim.value, 0),
-                          child: Container(
-                            height: 200,
-                            width: double.infinity,
-                            margin: const EdgeInsets.all(20),
-                            decoration: BoxDecoration(
-                              borderRadius: BorderRadius.circular(16),
-                              border: Border.all(color: Colors.red, width: 2),
-                              color: Colors.red.withOpacity(0.1),
-                            ),
-                          ),
-                        ),
-                      ),
-                      Column(
-                          children: List.generate(
-                              4,
-                              (index) => AnimatedBuilder(
-                                    animation: oldAnswersAnim[index],
-                                    builder: (_, __) => Transform.translate(
-                                      offset: Offset(oldAnswersAnim[index].value, 0),
-                                      child: Container(
-                                        height: 60,
-                                        width: double.infinity,
-                                        margin: const EdgeInsets.all(20),
-                                        decoration: BoxDecoration(
-                                          borderRadius: BorderRadius.circular(16),
-                                          border: Border.all(color: Colors.red, width: 2),
-                                          color: Colors.red.withOpacity(0.1),
-                                        ),
-                                      ),
-                                    ),
-                                  ))),
-                    ],
-                  ),
-
-                  Column(
-                    children: [
-                      AnimatedBuilder(
-                        animation: newQuestion,
-                        builder: (_, __) => Transform.translate(
-                          offset: Offset(newQuestionAnim.value, 0),
-                          child: Container(
-                            height: 200,
-                            width: double.infinity,
-                            margin: const EdgeInsets.all(20),
-                            decoration: BoxDecoration(
-                              borderRadius: BorderRadius.circular(16),
-                              border: Border.all(color: Colors.blue, width: 2),
-                              color: Colors.blue.withOpacity(0.1),
-                            ),
-                          ),
-                        ),
-                      ),
-                      Column(
-                          children: List.generate(
-                              4,
-                              (index) => AnimatedBuilder(
-                                    animation: newAnswersAnim[index],
-                                    builder: (_, __) => Transform.translate(
-                                      offset: Offset(newAnswersAnim[index].value, 0),
-                                      child: Container(
-                                        height: 60,
-                                        width: double.infinity,
-                                        margin: const EdgeInsets.all(20),
-                                        decoration: BoxDecoration(
-                                          borderRadius: BorderRadius.circular(16),
-                                          border: Border.all(color: Colors.blue, width: 2),
-                                          color: Colors.blue.withOpacity(0.1),
-                                        ),
-                                      ),
-                                    ),
-                                  ))),
-                    ],
-                  ),
+                  ElevatedButton(onPressed: () => setState(() => currentIndex = (currentIndex - 1).clamp(0, itemCount - 1)), child: const Text('Left')),
+                  const SizedBox(width: 100),
+                  ElevatedButton(onPressed: () => setState(() => currentIndex = (currentIndex + 1).clamp(0, itemCount - 1)), child: const Text('Right')),
                 ],
               ),
-            )),
-            Padding(
-              padding: const EdgeInsets.all(10),
-              child: Row(
-                children: [
-                  Expanded(
-                      child: MainButton(
-                    title: 'REVERSE',
-                    padding: const EdgeInsets.symmetric(vertical: 15),
-                    borderSide: const BorderSide(color: Colors.red, width: 1),
-                    backgroundColor: Colors.white,
-                    textColor: Colors.black,
-                    onPressed: () {
-                      // nextController.reverse();
-                      // Future.delayed(const Duration(milliseconds: 100), () {
-                      //   prevController.reverse();
-                      // });
-
-                      for (int i = newAnswers.length - 1; i >= 0; i--) {
-                        Future.delayed(Duration(milliseconds: (newAnswers.length - i - 1) * 100), () => newAnswers[i].reverse());
-                      }
-                      Future.delayed(const Duration(milliseconds: 400), () => newQuestion.reverse());
-
-                      Future.delayed(const Duration(milliseconds: 400), () {
-                        for (int i = oldAnswers.length - 1; i >= 0; i--) {
-                          Future.delayed(Duration(milliseconds: (oldAnswers.length - i - 1) * 100), () => oldAnswers[i].reverse());
-                        }
-                        Future.delayed(const Duration(milliseconds: 400), () => oldQuestion.reverse());
-                      });
-                    },
-                  )),
-                  const SizedBox(width: 20),
-                  Expanded(
-                      child: MainButton(
-                    title: 'FORWARD',
-                    padding: const EdgeInsets.symmetric(vertical: 15),
-                    borderSide: const BorderSide(color: Colors.green, width: 1),
-                    backgroundColor: Colors.white,
-                    textColor: Colors.black,
-                    onPressed: () {
-                      // prevController.forward();
-                      // Future.delayed(const Duration(milliseconds: 100), () {
-                      //   nextController.forward();
-                      // });
-                      oldQuestion.forward();
-                      for (int i = 0; i < oldAnswers.length; i++) {
-                        Future.delayed(Duration(milliseconds: (i + 1) * 100), () => oldAnswers[i].forward());
-                      }
-
-                      Future.delayed(const Duration(milliseconds: 400), () {
-                        newQuestion.forward();
-                        for (int i = 0; i < newAnswers.length; i++) {
-                          Future.delayed(Duration(milliseconds: (i + 1) * 100), () => newAnswers[i].forward());
-                        }
-                      });
-                    },
-                  )),
-                ],
+              const SizedBox(height: 50),
+              Row(
+                children: List.generate(
+                    itemCount,
+                    (index) => Expanded(
+                            child: Container(
+                          height: 10,
+                          color: index % 2 == 0 ? Colors.white : Colors.red,
+                          child: Center(
+                            child: CircleAvatar(
+                              backgroundColor: index % 2 == 0 ? Colors.red : Colors.white,
+                              radius: 2,
+                            ),
+                          ),
+                        ))),
               ),
-            )
-          ],
-        ),
-      ),
-    );
+              const SizedBox(height: 20),
+            ],
+          ),
+        )),
+        Container(
+            height: 120,
+            width: double.infinity,
+            color: Colors.red,
+            child: Stack(
+              alignment: Alignment.bottomCenter,
+              children: [
+                Positioned(left: 0, child: Container(height: 80, width: (sideBoxWidth + offset).clamp(0, 10000), color: Colors.brown)),
+                Transform.translate(offset: Offset(offset, 0), child: _mainItem()),
+                Positioned(right: 0, child: Container(height: 80, width: (sideBoxWidth - offset).clamp(0, 10000), color: Colors.brown)),
+                Row(
+                  children: List.generate(
+                      itemCount,
+                      (index) => GestureDetector(
+                            onTap: () {
+                              setState(() {
+                                currentIndex = index;
+                              });
+                            },
+                            child: Container(
+                              height: 120,
+                              width: MediaQuery.of(context).size.width / itemCount,
+                              color: Colors.transparent,
+                            ),
+                          )),
+                ),
+                Transform.translate(
+                  offset: const Offset(0, -15),
+                  child: Row(
+                    children: List.generate(
+                      itemCount,
+                      (index) => SizedBox(
+                          width: MediaQuery.of(context).size.width / itemCount,
+                          child: Text(
+                            'item $index',
+                            textAlign: TextAlign.center,
+                            style: TextStyle(color: currentIndex == index ? Colors.white : Colors.white.withOpacity(0.5)),
+                          )),
+                    ),
+                  ),
+                )
+              ],
+            ))
+      ],
+    ));
   }
 
-  _buildDummyQuestion(Color color) => Column(
+  _mainItem() => Stack(
+        alignment: Alignment.bottomCenter,
         children: [
-          Container(
-            height: 200,
-            width: double.infinity,
-            margin: const EdgeInsets.all(20),
-            decoration: BoxDecoration(borderRadius: BorderRadius.circular(16), border: Border.all(color: color, width: 2), color: color.withOpacity(0.1)),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                height: 80,
+                width: 100,
+                decoration: const BoxDecoration(color: Colors.brown, borderRadius: BorderRadius.only(topRight: Radius.circular(100))),
+              ),
+              const SizedBox(width: 18),
+              Container(
+                height: 80,
+                width: 100,
+                decoration: const BoxDecoration(color: Colors.brown, borderRadius: BorderRadius.only(topLeft: Radius.circular(100))),
+              ),
+            ],
           ),
-          Column(
-              children: List.generate(
-                  4,
-                  (index) => Container(
-                        height: 60,
-                        width: double.infinity,
-                        margin: const EdgeInsets.all(20),
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(16),
-                          border: Border.all(color: color, width: 2),
-                          color: color.withOpacity(0.1),
-                        ),
-                      ))),
+          Container(width: 80, height: 50, color: Colors.brown),
+          Transform.translate(
+            offset: const Offset(0, -40),
+            child: const CircleAvatar(
+              radius: 40,
+              backgroundColor: Colors.red,
+              child: CircleAvatar(
+                radius: 35,
+                backgroundColor: Colors.brown,
+              ),
+            ),
+          ),
         ],
       );
 }
