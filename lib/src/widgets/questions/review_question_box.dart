a7e993a04075065229274d371d7de83b97988996
Hiep Nguyen <hiepnx27@gmail.com>
2024-09-16 15:05:37 +0700

    many changes

--- lib/src/widgets/questions/review_question_box.dart
+++ lib/src/widgets/questions/review_question_box.dart
@@ -59,6 +59,7 @@ class ReviewQuestionBox extends StatefulWidget {
 
   final Widget Function(BuildContext context, String text, TextStyle textStyle)? renderTextBuilder;
   final Widget Function(BuildContext context, String image)? renderImageBuilder;
+  final Widget Function(BuildContext context, TextStyle textStyle)? paragraphBuilder;
 
   // Callbacks
   final void Function(bool isSelected) onBookmark;
@@ -90,6 +91,7 @@ class ReviewQuestionBox extends StatefulWidget {
     this.topBackgroundColor = const Color(0xFFFFFDF1),
     this.renderTextBuilder,
     this.renderImageBuilder,
+    this.paragraphBuilder,
   });
 
   @override
@@ -173,6 +175,8 @@ class _ReviewQuestionBoxState extends State<ReviewQuestionBox> {
                     '${widget.index + 1}. ${widget.questionData.question}',
                     style: textStyle,
                   ),
+                if(widget.paragraphBuilder != null) 
+                  widget.paragraphBuilder!.call(context, textStyle),
                 Column(
                   children: List.generate(widget.questionData.answers.length,
                       (index) => _buildAnswer(widget.questionData.answers[index].content, isCorrect: widget.questionData.answers[index].isCorrect)),
